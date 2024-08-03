fed2c05ca13fc9c8384ff149b17d40db8c0a0844
CamHirsch23 <hirschboeck13@gmail.com>
2024-08-03 13:13:27 -0500

    Update App.js

--- App.js
+++ App.js
@@ -3,43 +3,10 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import CreateCustomerForm from './components/CreateCustomerForm';
 import CustomerDetails from './components/CustomerDetails';
 import UpdateCustomerForm from './components/UpdateCustomerForm';
-
-const App = () => {
-  return (
-    <Router>
-      <Switch>
-        <Route path="/create-customer" component={CreateCustomerForm} />
-        <Route path="/customer/:id" component={CustomerDetails} />
-        <Route path="/update-customer/:id" component={UpdateCustomerForm} />
-      </Switch>
-    </Router>
-  );
-};
-
-export default App;
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import CreateProductForm from './components/CreateProductForm';
 import ProductList from './components/ProductList';
 import ProductDetails from './components/ProductDetails';
 import UpdateProductForm from './components/UpdateProductForm';
-
-const App = () => {
-  return (
-    <Router>
-      <Switch>
-        <Route path="/create-product" component={CreateProductForm} />
-        <Route path="/products" component={ProductList} />
-        <Route path="/product/:id" component={ProductDetails} />
-        <Route path="/update-product/:id" component={UpdateProductForm} />
-      </Switch>
-    </Router>
-  );
-};
-
-export default App;
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import PlaceOrderForm from './components/PlaceOrderForm';
 import OrderDetails from './components/OrderDetails';
 import TrackOrderStatus from './components/TrackOrderStatus';
@@ -48,9 +15,25 @@ const App = () => {
   return (
     <Router>
       <Switch>
+        {/* Customer Routes */}
+        <Route path="/create-customer" component={CreateCustomerForm} />
+        <Route path="/customer/:id" component={CustomerDetails} />
+        <Route path="/update-customer/:id" component={UpdateCustomerForm} />
+        
+        {/* Product Routes */}
+        <Route path="/create-product" component={CreateProductForm} />
+        <Route path="/products" component={ProductList} />
+        <Route path="/product/:id" component={ProductDetails} />
+        <Route path="/update-product/:id" component={UpdateProductForm} />
+        
+        {/* Order Routes */}
         <Route path="/place-order" component={PlaceOrderForm} />
         <Route path="/order/:id" component={OrderDetails} />
         <Route path="/track-order/:id" component={TrackOrderStatus} />
+        
+        {/* You can add a default route for home or a not-found page */}
+        {/* <Route exact path="/" component={Home} /> */}
+        {/* <Route component={NotFound} /> */}
       </Switch>
     </Router>
   );
