99fe7850262f958eafc3424baf699c964e89bb7d
CamHirsch23 <hirschboeck13@gmail.com>
2024-08-03 12:38:18 -0500

    Update Services.js

--- Services.js
+++ Services.js
@@ -37,3 +37,51 @@ export const deleteCustomer = async (id) => {
     throw error;
   }
 };
+import axios from 'axios';
+
+const API_BASE_URL = 'http://yourapi.com/api';
+
+export const createProduct = async (productData) => {
+  try {
+    const response = await axios.post(`${API_BASE_URL}/products`, productData);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const getProducts = async () => {
+  try {
+    const response = await axios.get(`${API_BASE_URL}/products`);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const getProduct = async (id) => {
+  try {
+    const response = await axios.get(`${API_BASE_URL}/products/${id}`);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const updateProduct = async (id, productData) => {
+  try {
+    const response = await axios.put(`${API_BASE_URL}/products/${id}`, productData);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const deleteProduct = async (id) => {
+  try {
+    const response = await axios.delete(`${API_BASE_URL}/products/${id}`);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
