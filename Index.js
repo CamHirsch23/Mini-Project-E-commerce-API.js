bb4d03be7e221450cf32642f7e026d6a930e8339
CamHirsch23 <hirschboeck13@gmail.com>
2024-08-03 12:37:05 -0500

    Update Index.js

--- Index.js
+++ Index.js
@@ -9,3 +9,14 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
